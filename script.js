b72214c587dd3674b7c2ffccfb9f069b53b0d57e
Mahabodhimpbv <145769325+Mahabodhimpbv@users.noreply.github.com>
2023-09-22 17:08:09 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,38 @@
+var prevScrollpos = window.pageYOffset;
+var header = document.querySelector(".navbar");
+var headerText = document.querySelectorAll(".fade");
+var bottomElement = document.querySelector(".bottom");
+
+window.onscroll = function() {
+    var currentScrollPos = window.pageYOffset;
+
+    if (prevScrollpos > currentScrollPos) {
+        // Scrolling up, show the faded elements and bottom
+        header.style.top = "0";
+        headerText.forEach(function(element) {
+            element.style.opacity = "1";
+        });
+        bottomElement.style.opacity = "1";
+    } else {
+        // Scrolling down, hide the faded elements and bottom
+        header.style.top = "-" + header.clientHeight + "px";
+        headerText.forEach(function(element) {
+            element.style.opacity = "0";
+        });
+        bottomElement.style.opacity = "0";
+    }
+    prevScrollpos = currentScrollPos;
+};
+
+document.getElementById('menu-toggle').addEventListener('click', function() {
+    var menu = document.getElementById('menu');
+    var menu = document.getElementById('menu');
+    if (menu.classList.contains('closed')) {
+        menu.style.right = '0'; // Open the menu
+        menu.classList.remove('closed');
+    } else {
+        menu.style.right = '-250px'; // Close the menu
+        menu.classList.add('closed');
+    }
+});
+
